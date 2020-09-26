8ae4ea3e48f25dfcfeffe9d86c90de3772d48f65
BuildTools <unconfigured@null.spigotmc.org>
2020-09-27 00:33:51 +0300

    Добавьте файлы проекта.

---
+++ coursework_1/coursework_1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "practical_work_1.h"
+
+using namespace std;
+
+int main()
+{
+    practical_work_1 fgjfgj;
+    fgjfgj.cout_weight_data_types();
+    system("pause");
+}
---
+++ coursework_1/practical_work_1.h
@@ -0,0 +1,13 @@
+#pragma once
+class practical_work_1
+{
+public:
+	practical_work_1();
+	~practical_work_1();
+	void cout_weight_data_types();
+	void cout_bin_representation_int_data_types() {};
+	void cout_bin_representation_float_data_types() {};
+	void cout_bin_representation_double_data_types() {};
+
+private:
+};
